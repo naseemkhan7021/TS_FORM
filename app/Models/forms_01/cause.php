167ee974375e650527da4fe82479e8bc9c87fa71
SALEEM SHAH <softkids2000@gmail.com>
2021-12-02 13:09:56 +0530

    Models Updated

--- app/Models/forms_01/cause.php
+++ app/Models/forms_01/cause.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class cause extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'causes_id';
+    protected $table = 'causes';
+    protected $fillable = [
+        'causes_description','causes_abbr'
+     ];
+
 }
